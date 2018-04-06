dafe2d9e6ae3c9237da3a569f9f4df3c052341f4
Dale Terrell <tragicid@gmail.com>
2018-04-06 16:15:51 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,7 @@
 def display_rainbow(colors)
   #color = ['red', 'orange']
   # puts "#{color[0]}" because color[0] = 'red' >> "red"
-  puts "R: #{colors[0]}, O: #{colors[1]}, "Y: #{colors[2]}, G: #{colors[3]}", B: #{colors[4]}", I: #{colors[5]}", V: #{colors[6]}"
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 
 end
 #puts "The colors of the rainbow are #{display_rainbow}"
